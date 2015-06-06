972ef2b95cecfc388c4804cd604e03b5c3f9fd97
Carlos Mendes <carlosepmendes@gmail.com>
2015-06-06 00:28:09 +0000

    Added layout and test with toasts

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,4 +38,15 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClick(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch the XYZ reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
 }
